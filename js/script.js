35d561fa9abe4668a13ea0449774ec882c515cda
Sander <sandervdb23@gmail.com>
2024-08-13 19:55:59 +0700

    ganti input TB, nambah gender di alert

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-/// ini javascript
+function hitungBMI() {
+    let height = document.getElementById("height").value;
+    let weight = document.getElementById("weight").value;
+
+    let hasil = parseInt(weight) / (parseInt(height) / 100) ** 2;
+
+    if(document.getElementById('pria').checked == true) {   
+        var gender = "Pria"
+    } else if(document.getElementById('wanita').checked == true) {  
+        var gender = "Wanita" 
+    };
+    
+    alert(gender + " " + "dengan hasil BMI" + " " + hasil)
+}
+  
